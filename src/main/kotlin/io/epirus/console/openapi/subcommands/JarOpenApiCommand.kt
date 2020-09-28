28fd4cea3651a191206df871c40d38b9a0eb3585
rachid chami <chamirachid1@gmail.com>
2020-09-28 15:24:38 +0100

    Adds openapi jar functionality

--- src/main/kotlin/io/epirus/console/openapi/subcommands/JarOpenApiCommand.kt
+++ src/main/kotlin/io/epirus/console/openapi/subcommands/JarOpenApiCommand.kt
@@ -13,9 +13,17 @@
 package io.epirus.console.openapi.subcommands
 
 import io.epirus.console.EpirusVersionProvider
+import io.epirus.console.openapi.OpenApiGeneratorService
+import io.epirus.console.openapi.OpenApiGeneratorServiceConfiguration
 import io.epirus.console.openapi.options.PreCompiledContractOptions
+import io.epirus.console.openapi.utils.GradleUtils
+import io.epirus.console.openapi.utils.PrettyPrinter
+import io.epirus.console.openapi.utils.SimpleFileLogger
 import picocli.CommandLine
 import java.io.File
+import java.nio.file.Files
+import java.nio.file.Paths
+import java.nio.file.StandardCopyOption
 
 @CommandLine.Command(
     name = "jar",
@@ -35,5 +43,51 @@ class JarOpenApiCommand : AbstractOpenApiCommand() {
     val preCompiledContractOptions = PreCompiledContractOptions()
 
     override fun generate(projectFolder: File) {
+        if (preCompiledContractOptions.abis.isEmpty()) {
+            print("\nGenerating Hello World OpenAPI JAR ...\n")
+        } else {
+            print("\nGenerating OpenAPI JAR ...\n")
+        }
+        SimpleFileLogger.startLogging()
+        val tempFolderPath = Paths.get(projectFolder.toString(), projectOptions.projectName)
+
+        OpenApiGeneratorService(
+            OpenApiGeneratorServiceConfiguration(
+                projectName = projectOptions.projectName,
+                packageName = projectOptions.packageName,
+                outputDir = tempFolderPath.toString(),
+                abis = preCompiledContractOptions.abis,
+                bins = preCompiledContractOptions.bins,
+                addressLength = projectOptions.addressLength,
+                contextPath = projectOptions.contextPath?.removeSuffix("/") ?: projectOptions.projectName
+            )
+        ).generate()
+
+        GradleUtils.runGradleTask(
+            tempFolderPath.toFile(),
+            "shadowJar",
+            emptyList(),
+            "Generating the Jar ...",
+            System.out
+        )
+
+        Files.copy(
+            getJarFile(tempFolderPath.toFile()).toPath(),
+            File(projectOptions.outputDir, "${projectOptions.projectName}-$JARSUFFIX").toPath(),
+            StandardCopyOption.REPLACE_EXISTING
+        )
+
+        PrettyPrinter.onJarSuccess()
+    }
+
+    private fun getJarFile(outputProjectFolder: File): File {
+        return File(
+            Paths.get(
+                outputProjectFolder.toString(),
+                "server",
+                "build",
+                "libs"
+            ).toString()
+        ).listFiles()!!.first { it.name.endsWith("-all.jar") }
     }
 }
