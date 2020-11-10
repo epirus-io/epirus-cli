065785eb56b12a3dc92293334701ff509732b815
rachid chami <chamirachid1@gmail.com>
2020-11-10 16:25:20 +0100

    spotless

--- src/main/kotlin/io/epirus/console/openapi/project/erc777/CopyUtils.kt
+++ src/main/kotlin/io/epirus/console/openapi/project/erc777/CopyUtils.kt
@@ -16,7 +16,6 @@ import io.epirus.console.project.templates.TemplateReader
 import java.io.IOException
 import java.nio.file.Files
 import java.nio.file.Path
-import java.nio.file.Paths
 
 internal object CopyUtils {
     /**
